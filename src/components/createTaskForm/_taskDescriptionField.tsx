28f09993fe04509483196302cc2200e58f8f4ad4
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2023-11-13 09:47:44 +0400

    Added MUI, added components for sidebar and create task form

---
+++ src/components/createTaskForm/_taskDescriptionField.tsx
@@ -0,0 +1,34 @@
+import React, { FC, ReactElement } from 'react';
+import { TextField } from '@mui/material';
+import { ITextField } from './interfaces/ITextField';
+import PropTypes from 'prop-types';
+
+export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
+  const {
+    onChange = (e) => {
+      console.log(e);
+    },
+    disabled = false,
+  } = props;
+
+  return (
+    <TextField
+      id="description"
+      label="Description"
+      placeholder="Description"
+      variant="outlined"
+      size="small"
+      name="title"
+      fullWidth
+      multiline
+      rows={4}
+      disabled={disabled}
+      onChange={onChange}
+    />
+  );
+};
+
+TaskDescriptionField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool
+}
